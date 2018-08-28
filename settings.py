3b90244135c30d47ec6b43e0cefefc007a1c870d
wangdaodao7 <ahking0516@126.com>
2018-08-28 21:08:25 +0800

    uuuuu

--- settings.py
+++ settings.py
@@ -3,6 +3,6 @@ __author__ = 'wangdaodao'
 
 
 
-MONGO_URL = '192.168.30.11'
+MONGO_URL = 'localhost'
 MONGO_DB = 'mychat'
 MONGO_TABLE = 'friends'
--- to_mongo.py
+++ to_mongo.py
@@ -7,12 +7,13 @@ from settings import MONGO_DB, MONGO_URL, MONGO_TABLE
 
 
 class MongoPipeline(object):
-    conn = pymongo.mongo_client(MONGO_URL)
+    conn = pymongo.MongoClient(MONGO_URL)
 
     def to_mongo(self, item):
         collection_name = MONGO_TABLE
         db = self.conn[MONGO_DB]
         db[collection_name].insert(dict(item))
+        print('done!')
 
     def form_mongo(self):
         collection_name = MONGO_TABLE
@@ -20,8 +21,10 @@ class MongoPipeline(object):
         collection = db.get_collection(collection_name)
         document = collection.find()
         return document
+
     def close_mongo(self):
         self.conn.close()
-        
-        
-MongoPipeline()
+
+
+# x = MongoPipeline()
+# x.to_mongo({'name':22222, 'sex':'female'})
