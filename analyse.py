425220f9cc3ff958895856fbb3269823accad985
wangdaodao7 <ahking0516@126.com>
2018-08-30 21:27:46 +0800

    uuuu

--- analyse.py
+++ analyse.py
@@ -9,3 +9,25 @@ import os
 import math
 
 import PIL.Image as Image
+
+
+
+def mergeImage():
+    photo_width = 50
+    photo_height = 50
+    photo_path_list = []
+    dirName = os.getcwd() + '/images'
+
+    for root, dirs, files in os.walk(dirName):
+        # print(root, dirs, files)
+        for file in files:
+            if 'jpg' in file and os.path.getsize(os.path.join(root, file)) > 0:
+                photo_path_list.append(os.path.join(root, file))
+            elif 'jpg' in file and os.path.getsize(os.path.join(root, file)) = 0:
+                photo_path_list.append(os.path.join(root, file))
+
+
+
+
+
+mergeImage()
