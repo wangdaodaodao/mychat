c32f151021fa0ca93eadabee0d74f5e53890c1ed
wangdaodao7 <ahking0516@126.com>
2018-09-02 10:44:58 +0800

    ......

--- analyse.py
+++ analyse.py
@@ -2,14 +2,11 @@
 __author__ = 'wangdaodao'
 
 
-
-import  json
+import json
 import re
 import os
 import math
-
 import PIL.Image as Image
-
 
 
 def mergeImage():
@@ -19,35 +16,34 @@ def mergeImage():
     dirName = os.getcwd() + '/images'
 
     for root, dirs, files in os.walk(dirName):
-        # print(root, dirs, files)
         for file in files:
             if 'jpg' in file and os.path.getsize(os.path.join(root, file)) > 0:
                 photo_path_list.append(os.path.join(root, file))
             elif 'jpg' in file and os.path.getsize(os.path.join(root, file)) == 0:
                 photo_path_list.append(os.path.join(root, file))
-    
+
     pic_num = len(photo_path_list)
-    line_max = int(math.sqrt(pic_num))-2
+    line_max = int(math.sqrt(pic_num)) - 2
     row_max = int(math.sqrt(pic_num)) + 4
     print(line_max, row_max, pic_num)
-    
-    if line_max >20:
-        line_max =20
+
+    if line_max > 20:
+        line_max = 20
         row_max = 20
 
-    
     num = 0
     pic_max = line_max * row_max
-    toImage = Image.new('RGBA', (photo_width*line_max, photo_height*row_max))
+    toImage = Image.new(
+        'RGBA', (photo_width * line_max, photo_height * row_max))
 
     for i in range(0, row_max):
         for j in range(0, line_max):
             pic_fole_head = Image.open(photo_path_list[num])
-            
             tmppic = pic_fole_head.resize((photo_width, photo_height))
-            print(j%line_max*photo_width, row_max ,photo_height, photo_width)
-
-            loc = (int(j%line_max*photo_width), int(i%row_max*photo_height))
+            print(j % line_max * photo_width,
+                  row_max, photo_height, photo_width)
+            loc = (int(j % line_max * photo_width),
+                   int(i % row_max * photo_height))
             toImage.paste(tmppic, loc)
             num += 1
             if num >= len(photo_path_list):
@@ -55,11 +51,7 @@ def mergeImage():
         if num >= pic_max:
             break
         print(toImage.size)
-    
     toImage.save('head_photo.png')
-
-
-
 
 
 mergeImage()
