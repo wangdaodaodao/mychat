164659a33e9ca262d7e42dce1159d825fb333697
wangdaodao7 <ahking0516@126.com>
2018-09-09 22:49:43 +0800

    UUU

--- analyse.py
+++ analyse.py
@@ -13,7 +13,6 @@ from collections import Counter
 import jieba.analyse
 import PIL.Image as Image
 
-
 from to_mongo import MongoPipeline
 
 
@@ -40,14 +39,15 @@ def get_pie(title, name_list, num_list):
     pie = Pie(title, title_text_size=30, title_pos='center',
               subtitle=subtitle, subtitle_text_size=25, width=800, height=800)
     pie.add('', name_list, num_list, is_label_show=True, center=[50, 45], radius=[
-            0, 50], legend='right', legend_orient='vertical', label_text_size=20)
+        0, 50], legend='right', legend_orient='vertical', label_text_size=20)
     out_file_name = './analyse/{}.html'.format(title)
     pie.render(out_file_name)
 
 def get_bar(title, name_list, num_list):
     bar = Bar(title, title_text_size=30, title_pos='center')
     bar.add('', name_list, num_list, title_pos='center', xaxis_interval=0, xaxis_rotate=27,
-            xaxis_label_textcolor=20, yaxis_label_textcolor=20, yaxis_name_pos='end', yaxis_pos='%50')
+            xaxis_label_textcolor=20, yaxis_label_textcolor=20, yaxis_name_pos='end',
+            yaxis_pos='%50')
     bar.show_config()
     # graph = Graph(width=1300, height=800)
     # graph.add(bar, graph_top='13%', graph_bottom='24%', graph_left='15%', graph_right='15%')
@@ -60,15 +60,13 @@ def get_map(title, name_list, num_list):
     out_file_name = './analyse/{}.html'.format(title)
     _map.render(out_file_name)
 
-def word_clout(title,name_list,num_list,word_size_range):
-    '''词云图'''
-
-    wordcloud = WordCloud(width=1400,height=900)
-
-    wordcloud.add("",name_list,num_list,word_size_range=word_size_range,shape='pentagon')
+def word_clout(title, name_list, num_list, word_size_range):
+    wordcloud = WordCloud(width=1400, height=900)
+    wordcloud.add("", name_list, num_list, word_size_range=word_size_range, shape='pentagon')
     out_file_name = './analyse/'+title+'.html'
     wordcloud.render(out_file_name)
-    
+
+
 def mergeImage():
     photo_width = 50
     photo_height = 50
@@ -100,7 +98,7 @@ def mergeImage():
         for j in range(0, line_max):
             pic_fole_head = Image.open(photo_path_list[num])
             tmppic = pic_fole_head.resize(
-                (photo_width, photo_height),  Image.ANTIALIAS)
+                (photo_width, photo_height), Image.ANTIALIAS)
             print(j % line_max * photo_width,
                   row_max, photo_height, photo_width)
             loc = (int(j % line_max * photo_width),
