1a2ff7597273d8ac3473e084972f64d802965c31
wangdaodao7 <ahking0516@126.com>
2018-09-11 22:16:22 +0800

    uuu

--- duoxiancheng.py
+++ duoxiancheng.py
@@ -33,26 +33,21 @@ class Spider():
         pageinfo = selector.xpath(
             '//header[@class="listing-header"]/h1[1]/text()')
         string = str(pageinfo[0])
-        # print(string)
         numlist = list(filter(str.isdigit, string))
-        # print(numlist)
         for item in numlist:
             total += item
             totalpagenum = int(total)
-        # print(totalpagenum)
         return totalpagenum
 
     def get_links(self, number):
         url = "https://alpha.wallhaven.cc/search?q={}&categories=111&purity=100&sorting=relevance&order=desc&page={}".format(
             keyword, number)
-        # print(url)
         try:
             html = requests.get(url, headers=self.headers)
             selector = etree.HTML(html.text)
             pic_linklist = selector.xpath(
                 '//a[@class="jsAnchor thumb-tags-toggle tagged"]/@href')
             return pic_linklist
-            # print(pic_linklist)
         except Exception as e:
             print(e)
 
@@ -61,8 +56,6 @@ class Spider():
             '/thumbTags').strip('https://alpha.wallhaven.cc/wallpaper/')
         print(string)
         html = 'http://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-' + string + '.jpg'
-
-        # print(html)
         pic_path = (self.filepath + keyword + str(count) + '.jpg')
         if os.path.exists(pic_path):
             print('文件已存在！')
@@ -84,25 +77,19 @@ class Spider():
         times = int(count / 24 + 1)
         j = 1
         start = time.time()
-        # x = self.get_links(j)
-        # print(x)
         for j in range(times):
             pic_urls = self.get_links(j)
             print(pic_urls)
-            start2 = time.time()
             threads = []
             for item in pic_urls:
-                print(item)
                 t = Thread(target=self.download, args=[item, j])
                 t.start()
                 threads.append(t)
                 j += 1
             for t in threads:
                 t.join()
-            end2 = time.time()
-            # time.sleep(3)
         end = time.time()
-        print('总耗时：{}'.format(end - start2))
+        print('总耗时：{}'.format(end - start))
 
 
 spider = Spider()
