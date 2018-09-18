731e96917bddb36bdc9eb877b30cb5308290d1c8
wangdaodaodao <ahking0516@126.com>
2018-09-18 15:07:45 +0800

    uuuu

--- guess.py
+++ guess.py
@@ -5,7 +5,7 @@ import math
 
 
 def guess_one():
-    the_one = 10000
+    the_one = 100000000
     one = random.randint(1 ,the_one)
     print(one)
     bingo = True
@@ -17,15 +17,16 @@ def guess_one():
         if guess_one == one - 1 :
             print('第--{}--次猜对了，{}={}'.format(i, guess_one+1, one))
             bingo = False
+
         elif guess_one == one + 1 :
             print('第--{}--次猜对了，{}={}'.format(i, guess_one-1, one))
             bingo = False    
+
         elif guess_one < one:
             print('第--{}--次猜数字是<{}>,小了'.format(i, guess_one), end='\t')
             # the_big_one = the_one
             the_small_one = guess_one
             print('[{}-{}]'.format(the_small_one, the_big_one))
-
             guess_one = int(guess_one + (the_big_one - guess_one)/2 + 1)
             print('猜错了，重新猜{}'.format(guess_one))
             
@@ -36,13 +37,12 @@ def guess_one():
             guess_one = int( (the_small_one + guess_one)/2)
             print('猜错了，重新猜{}'.format(guess_one))
         
-
         elif guess_one == one:
             print('第--{}--次猜对了，{}={}'.format(i, guess_one, one))
             bingo = False
         i += 1
     print('总共猜了<<<{}>>>次'.format(i-1))
-    return i
+    return i-1
     
     
 
