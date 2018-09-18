558a93f503bd6a8d4842c369cb8d212f8d657f69
wangdaodaodao <ahking0516@126.com>
2018-09-18 15:05:31 +0800

    uuuuuu

--- guess.py
+++ guess.py
@@ -5,20 +5,23 @@ import math
 
 
 def guess_one():
-    the_one = 100
+    the_one = 10000
     one = random.randint(1 ,the_one)
     print(one)
     bingo = True
     i = 1
-
     the_small_one = 1
     the_big_one = the_one
-
     guess_one = int(the_one/2)
-
     while bingo:
-        if guess_one < one:
-            print('第--{}--次猜数字是<{}>,小了'.format(i, guess_one), )
+        if guess_one == one - 1 :
+            print('第--{}--次猜对了，{}={}'.format(i, guess_one+1, one))
+            bingo = False
+        elif guess_one == one + 1 :
+            print('第--{}--次猜对了，{}={}'.format(i, guess_one-1, one))
+            bingo = False    
+        elif guess_one < one:
+            print('第--{}--次猜数字是<{}>,小了'.format(i, guess_one), end='\t')
             # the_big_one = the_one
             the_small_one = guess_one
             print('[{}-{}]'.format(the_small_one, the_big_one))
@@ -27,20 +30,18 @@ def guess_one():
             print('猜错了，重新猜{}'.format(guess_one))
             
         elif guess_one > one:
-            print('第--{}--次猜数字是<{}>，大了'.format(i, guess_one))
+            print('第--{}--次猜数字是<{}>，大了'.format(i, guess_one), end='\t')
             the_big_one = guess_one
             print('[{}-{}]'.format(the_small_one, the_big_one), )
             guess_one = int( (the_small_one + guess_one)/2)
             print('猜错了，重新猜{}'.format(guess_one))
-        elif guess_one == one + 1:
-            pass
+        
 
         elif guess_one == one:
-            print('猜对了，{}={}'.format(guess_one, one))
+            print('第--{}--次猜对了，{}={}'.format(i, guess_one, one))
             bingo = False
         i += 1
-    print('总共猜了<<<{}>>>次'.format(i))
-        # time.sleep(1)
+    print('总共猜了<<<{}>>>次'.format(i-1))
     return i
     
     
